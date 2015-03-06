9ab7941a42dd00b4f7b9086563a1f81a6aa4936e
seva <dragomirov@hotmail.com>
2015-03-07 00:08:04 +0300

    ordered lists 2015-03-06. First ok

--- headers/task.h
+++ headers/task.h
@@ -13,10 +13,6 @@
 #include <string.h>
 #endif
 
-/*#ifndef UTHASH_H
-#include "uthash.h"
-#endif*/
-
 #define MAXINT(x,y) ((x>y)?(x):(y))
 
 /*-- item section --*/
@@ -34,54 +30,22 @@ typedef struct item_t {
 } item_t;
 size_t ITEM_SIZE;
 
-/*typedef struct elem_t {
-  item_t *
-  struct elem_t *next, *prev;
-} elem_t;*/
-
 item_t* createitems (int);
 item_t* createitems0 (int);
 item_t* copyitem (item_t*);
 item_t* copyitems (int, item_t*);
-//item_t* copyhash (item_t*);
 item_t* joinitems (int, item_t*, int, item_t*);
 void print_items (int,item_t*);
 void print_items_line (int,item_t*);
-//void print_hash (item_t*);
 void print_items_list (item_t*);
 void free_items (item_t**);
-//void free_hash (item_t**);
 void free_items_list (item_t**);
 
 int put_item (item_t*, item_t *);
 item_t* find_preplace (item_t*, item_t*);
 item_t* find_preplace_badcutter (item_t*, item_t*); // find preplace and cut bad items with inefficient payoffs
 
-
-/*-- list for collect all solutions section --*/
-
-typedef struct node_list_t {
-  item_t *items;
-  int length;
-  struct node_list_t *next;
-} node_list_t;
-typedef struct head_list_t {
-  node_list_t *next;
-  int count;
-} head_list_t;
-node_list_t* createlistnode ();
-head_list_t* createlisthead ();
-
-void additems (head_list_t*, int, item_t*);
-/*	add adjunct's list to end of head's list
-*/	void addlist (head_list_t*, head_list_t*);
-void addnode (head_list_t*, node_list_t*);
-//items_list_t* addhead (items_list_t* head, int length , item_t*);
-void print_list (head_list_t*);
-void free_list(head_list_t**);
-
-
-/*-- task section --*/
+//-- task section ---
 
 typedef struct task{
   knint b;
@@ -95,7 +59,7 @@ void print_task (task_t*);
 
 void free_task (task_t**);
 
-/*-- tree section --*/
+// -- tree section ---
 
 typedef struct node {
   item_t  *items; // hash
@@ -111,24 +75,5 @@ void print_node ( char*, node_t* );
 
 void free_tree ( node_t* ) ;
 void free_node ( node_t* ) ;
-
-int value_sort ( item_t*, item_t* );
-
-/*-- solutions tree section --*/
-
-size_t SOLNODE_SIZE;
-
-typedef struct solnode_t {
-	item_t *items;
-	struct solnode_t *childs;
-	int level, branch;
-	UT_hash_handle hh;
-} solnode_t;
-solnode_t* createsolnode0 ();
-solnode_t* createsolnode (int, int);
-solnode_t* addsolchild ( solnode_t*, int, int );
-solnode_t* addsolitem ( solnode_t*, knint*, knint* );
-void print_solutions ( solnode_t* );
-void free_solnodes ( solnode_t* );
 
 #endif
